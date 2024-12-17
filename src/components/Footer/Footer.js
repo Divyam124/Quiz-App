963f3e0c97dce35814260e3c42ebd560778a957c
Divyam Modi <divyammodi063@gmail.com>
2024-12-17 18:14:31 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ♥ by Abhay Gupta
+    </div>
+  );
+};
+
+export default Footer;
